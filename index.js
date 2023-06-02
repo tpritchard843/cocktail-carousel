4cde8d46d6c23d1c87efcb54adf42845455f9160
Thomas Pritchard <110137519+tpritchard843@users.noreply.github.com>
2023-06-02 16:25:17 -0400

    Cocktail carousel feature (#1)
    * Created async function renderDrinkInfo()
    
    * Created async function renderDrinkInfo
    
    This feature loops through the fetched drinks array/category.
    Every 5 seconds, it updates the DOM with a new drink in that category.
    
    * Logic update to API call
    
    Added an if statement that immediately renders a drink to the DOM if the returned array has only one drink.
    
    * Moved index.js file
    
    * Changed fetch logic -- added else condition for async loop
    
    * Fixed render drink function
    This function now renders the first card in the cocktail carousel upon execution, before looping throught the rest every five seconds.
    
    * Fixed looping bug

---
+++ index.js
@@ -0,0 +1,43 @@
+//The user will enter a coktail. Our program will fetch cocktail name, photo, instructions, and ingredients and place them the DOM.
+
+document.querySelector('button').addEventListener('click', getDrink);
+
+function getDrink() {
+    let drink = document.querySelector('input').value;
+
+    const delay = (time) => {
+        return new Promise ((resolve) => setTimeout(resolve,time));
+    }
+
+    fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drink}`)
+    .then(res => res.json()) // parse response as JSON
+    .then(data => {
+        console.log(data.drinks);
+
+        if (data.drinks.length < 2) {
+            document.querySelector('.drink-name').innerText = data.drinks[0].strDrink; 
+            document.querySelector('.instructions-text').innerText = data.drinks[0].strInstructions;
+            document.querySelector('.drink-img').src = data.drinks[0].strDrinkThumb;
+        } else{
+            const renderDrinkInfo = async () => {
+                //high level stuff gets executed synchronously
+                //Render first cocktail card to the DOM
+                document.querySelector('.drink-name').innerText = data.drinks[0].strDrink; 
+                document.querySelector('.instructions-text').innerText = data.drinks[0].strInstructions;
+                document.querySelector('.drink-img').src = data.drinks[0].strDrinkThumb;
+
+                //Loop through the remaining drinks in the array at 5s intervals
+                for (let i = 1; i < data.drinks.length; i++) {
+                    await delay(3000);
+
+                    document.querySelector('.drink-name').innerText = data.drinks[i].strDrink; 
+                    document.querySelector('.instructions-text').innerText = data.drinks[i].strInstructions;
+                    document.querySelector('.drink-img').src = data.drinks[i].strDrinkThumb;
+    
+                    console.log(i, data.drinks[i]);
+                }
+            }
+            renderDrinkInfo();
+        } 
+    })
+}
